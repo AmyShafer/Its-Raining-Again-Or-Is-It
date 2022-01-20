db0b9b2472c742ce3c81741a4e66ab139e1d00fe
Amy Shafer <amy.u.shafer@gmail.com>
2022-01-20 13:38:50 -0500

    weather object is nearly there but UV Index is missing

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,6 @@ var APIkey = "414af75288c260f4c9c7eed4eff2b900";
 var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity},us&APPID=${APIkey}`;
 
 function getAPI () {
-  //var requestUrl = 'api';
 
   fetch(requestUrl) 
     .then(function (response) {
@@ -12,8 +11,29 @@ function getAPI () {
     })
     .then(function (data) {
         console.log(data);
+        weather = data;
+        currentWeatherSearch(weather);
     })
 }
+
+function currentWeatherSearch(weather) {
+  var weatherObj = {
+    temp: celsiusToFahrenheit(weather.main.temp),
+    wind: weather.wind.speed,
+    humidity: weather.main.humidity,
+    uvIdex: weather.main.uvIndex
+  }
+  console.log(weatherObj);
+}
+
+function pastWeatherSearches () {
+
+}
+
+function celsiusToFahrenheit (celTemp) {
+  var convertedTemp =  (celTemp * 9 )/ (5 + 32);
+  return Math.ceil(convertedTemp) + "°F";
+} 
 
 getWeatherBtn.addEventListener("click", getAPI());
 
