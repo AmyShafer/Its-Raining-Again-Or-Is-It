13e39aa1ef608df9145bc8bcde203b3cef03b726
Amy Shafer <amy.u.shafer@gmail.com>
2022-01-19 23:36:24 -0500

    bootstrap add

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var userCity = document.getElementById("#userCity");
+var userState = document.getElementById("#userState");
+var userZipCode = document.getElementById("#userZipCode");
+var APIkey = "29b72d6dff855141e3edfe32af26d163";
+
+// api.openweathermap.org/data/2.5/weather?q={userCity}&appid={APIkey}
+
+
