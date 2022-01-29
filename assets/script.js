89686792282949bcd0a1a6081bff3ed7738d4166
Amy Shafer <amy.u.shafer@gmail.com>
2022-01-29 17:22:07 -0500

    The dates and icons are appearing

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,41 @@
-var getWeatherBtn = document.getElementById("hallelujah");
-var APIkey = "414af75288c260f4c9c7eed4eff2b900";
-//var requestUrl = ;
+const getWeatherBtn = document.getElementById("hallelujah");
+const APIkey = "414af75288c260f4c9c7eed4eff2b900";
+const weatherIcon = document.querySelectorAll('.weather-icon');
 
 function getAPI (event) {
   event.preventDefault();
-  var userCity = document.getElementById("user-city").value;
+  let userCity = document.getElementById("user-city").value;
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userCity},us&appid=${APIkey}`) 
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       weather = data;
-      console.log(weather);
       currentWeatherSearch(weather);
     })
 }
 
 // The UV Index requires an One Call API request unlike the other information
 function uvIndex (weatherData) {
-  var oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${weatherData.lat}&lon=${weatherData.long}&appid=${APIkey}`;
+  let oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${weatherData.lat}&lon=${weatherData.long}&appid=${APIkey}`;
 
   fetch(oneCallAPI) 
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      var uvIndex = data.current.uvi;
+      let uvIndex = data.current.uvi;
+      let icon = data.current.weather[0].icon;
       weatherData.uvIndex = uvIndex;
-      console.log(data);
+      weatherData.icon = icon;
+      console.log(uvIndex + " -- " + icon);
       displayWeather(data);
-      return uvIndex;
     })
 }
 
 // Organizes all the needed information into an object
 function currentWeatherSearch(weather) {
-
-  var weatherObj = {
+  let weatherObj = {
     lat: weather.coord.lat,
     long: weather.coord.lon,
     icon: weather.weather[0].icon,
@@ -45,16 +44,21 @@ function currentWeatherSearch(weather) {
     humidity: weather.main.humidity,
     uvIndex: 0
   }
+  console.log("Icon: " + weatherObj.icon);
   uvIndex(weatherObj);
 }
 
 function displayWeather (weather) {
-  var getCards = document.querySelectorAll(".card");
-  console.log(getCards);
-  for (var i = 0; i < getCards.length; i++) {
-    var currentCard = getCards[i];
-    var dateString = moment.unix(weather.daily[i].dt).format("MMM Do YY");
+  console.log(weather);
+  let getCards = document.querySelectorAll(".card");
+  for (let i = 0; i < getCards.length; i++) {
+    const currentCard = getCards[i];
+    const dateString = moment.unix(weather.daily[i].dt).format("MMM Do YYYY");
+    const currentIcon = weather.daily[i].weather[0].icon;
+    //const {icon} = data.weather[0];
     currentCard.childNodes[1].textContent = dateString;
+    weatherIcon[i].setAttribute("src", `http://openweathermap.org/img/wn/${currentIcon}.png`);
+    //currentCard.childNodes[2].textContent = `https://openweathermap.org/img/w/${currentIcon}.png`;
   }
 }
 
@@ -66,7 +70,7 @@ function pastWeatherSearches () {
 
 // Convert the temperature Celsius to Fahrenheit
 function celsiusToFahrenheit (celTemp) {
-  var convertedTemp =  (celTemp * 9 )/ (5 + 32);
+  let convertedTemp =  (celTemp * 9 )/ (5 + 32);
   return Math.ceil(convertedTemp) + "°F";
 } 
 
