8787637d9cae2b4c8be1276532d5751ae81ef357
Amy Shafer <amy.u.shafer@gmail.com>
2022-02-01 02:01:00 -0500

    search history functionality nearly there

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,8 @@ const getWeatherBtn = document.getElementById("hallelujah");
 const APIkey = "414af75288c260f4c9c7eed4eff2b900";
 const weatherIcon = document.querySelectorAll('.weather-icon');
 const pastSearchesContainer = document.querySelector('.past-searches');
-const citiesSearchList = document.querySelector(".past-cities-list");
+const citiesSearchList = document.querySelector(".past-cities");
+const cityButtons = document.querySelectorAll(".btn-history");
 
 // This is an array of messages for the breaking news function
 const breakingNewsMessages = [
@@ -12,7 +13,8 @@ const breakingNewsMessages = [
 `<h2> <i class="fas fa-rainbow"></i> HAPPY BIRTHDAY TO WEATHER GIRL, IZORA ARMSTEAD! HALLELUJAH FOR IZORA! <i class="fas fa-rainbow"></i></h2>`,
 `<h2> <i class="fas fa-bolt"></i> IT'S RAINING MEN WAS RELEASED ON SEPTEMBER 10TH, 1982! TODAY, THE ICONIC SONG TURNS 40! <i class="fas fa-bolt"></i></h2>`,
 `<h2> <i class="fas fa-glasses"></i> HAPPY BIRTHDAY TO PAUL SHAFFER, THE CO-COMPOSER OF IT'S RAINING MEN! <i class="fas fa-glasses"></i></h2>`,
-`<h2> <i class="fas fa-cloud-moon"></i> HAPPY BIRTHDAY TO WEATHER GIRL, MARTHA WASH! HALLELUJAH FOR MARTHA! <i class="fas fa-cloud-moon"></i></h2>`
+`<h2> <i class="fas fa-cloud-moon"></i> HAPPY BIRTHDAY TO WEATHER GIRL, MARTHA WASH! HALLELUJAH FOR MARTHA! <i class="fas fa-cloud-moon"></i></h2>`,
+`<h2><i class="fas fa-cloud-sun-rain"></i> ENTER THE CITIES YOU WOULD LIKE TO VISIT AND THE WEATHER GIRLS WILL TELL YOU IF IT IS RAINING THERE <i class="fas fa-cloud-sun-rain"></i></h2>`
 ];
 
 // Outputs important days in the breaking news header
@@ -33,8 +35,10 @@ function breakingNews (forecast) {
   } else if (todayDate === "Dec 28th") {
     breaking.innerHTML = breakingNewsMessages[4];
   // Weather Update
-  } else { 
+  } else if (forecast) { 
     breaking.innerHTML = `TODAY YOU CAN EXPECT ${forecast.toUpperCase()}!`;
+  } else {
+    breaking.innerHTML = breakingNewsMessages[5];
   }
 }
 
@@ -80,7 +84,7 @@ function uvIndex (weatherData) {
 function uvColorCode () {
   let getUV = document.querySelectorAll(".uv-index");
   for (let i = 0; i < getUV.length; i++) {
-    const currentUV = getUV[i].textContent;
+    const currentUV = parseFloat(getUV[i].textContent);
     // the default color is green, if the UV Index is under 2
     if (currentUV >= 2 && currentUV < 6) {
       getUV[i].setAttribute("style", "background-color: var(--tall-blonde); color: var(--shaffer-shades);");
@@ -88,7 +92,9 @@ function uvColorCode () {
       getUV[i].setAttribute("style", "background-color: var(--celsius-orange); color: var(--shaffer-shades);");
     } else if (currentUV >= 8 && currentUV < 11) {
       getUV[i].setAttribute("style", "background-color: var(--fahrenheit-red);");
-    } 
+    } else {
+      getUV[i].setAttribute("style", "background-color: var(--jabara-green);");
+    }
   }
 }
 
@@ -115,7 +121,6 @@ function displayWeather (weather) {
     currentCard.setAttribute("style", "visibility: visible")
     // needed information for each item
     const dateString = moment.unix(weather.daily[i].dt).format("MMM Do YYYY");
-    const daysForecast = weather.daily[i].weather[0].main;
     const daysIcon = weather.daily[i].weather[0].icon;
     const daysTemp = weather.daily[i].temp.day;
     const daysWind = weather.daily[i].wind_speed;
@@ -130,29 +135,32 @@ function displayWeather (weather) {
     currentCard.children[1].children[1].children[3].children[0].textContent = daysUVIndex;
   }
   uvColorCode();
-  pastWeatherSearches();
 }
 
 // Saves user's past city inputs and makes them buttons
-function pastWeatherSearches (recentSearch, e) {
+function pastWeatherSearches (recentSearch) {
   cities.push(recentSearch);
-  localStorage.setItem("Past Searches", JSON.stringify(cities));
+  localStorage.setItem("Past Searches", cities);
+  recentSearch = recentSearch.toUpperCase();
   let citiesSearched = localStorage.getItem("Past Searches");
+  let currentCityText = recentSearch;
+  let currentCityButton = document.createElement("button");
+    
+  currentCityButton.setAttribute("class", ".btn-history");
+  currentCityButton.textContent = currentCityText;
+  citiesSearchList.appendChild(currentCityButton);
   
-    const currentCity = recentSearch.toUpperCase();
-    const pastCity = document.createElement("button");
-    pastCity.setAttribute("class", ".btn-history");
-    pastCity.textContent = currentCity;
-
-    citiesSearchList.appendChild(pastCity);
 }
 
 // Converts the temperature Celsius to Fahrenheit
 function celsiusToFahrenheit (celTemp) {
   let convertedTemp =  (celTemp * 9 )/ (5 + 32);
   return Math.ceil(convertedTemp) + "°F";
-} 
+}
+
 window.addEventListener("load", breakingNews)
 getWeatherBtn.addEventListener("click", getAPI);
-pastSearchesContainer.addEventListener("click", pastWeatherSearches);
-
+citiesSearchList.addEventListener("click", function(event) {
+  let cityName = event.target.textContent.toLowerCase();
+  getAPI();
+});
