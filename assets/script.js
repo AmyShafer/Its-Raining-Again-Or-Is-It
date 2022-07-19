6c818f90cd6ea60519ab71669c946bc2dc9cbf9f
Amy Shafer <amy.u.shafer@gmail.com>
2022-07-18 20:02:51 -0400

    weather conversion correction

--- assets/script.js
+++ assets/script.js
@@ -5,41 +5,42 @@ const APIkey = "414af75288c260f4c9c7eed4eff2b900";
 const weatherIcon = document.querySelectorAll('.weather-icon');
 const citiesSearchList = document.querySelector(".past-cities");
 let forecast = "MAGIC";
+let weather = {};
 
 // This is an array of messages for the breaking news function
 const breakingNewsMessages = [
-`<h2> <i class="fas fa-music"></i> HAPPY BIRTHDAY TO PAUL JABARA, THE CO-COMPOSER OF IT'S RAINING MEN! <i class="fas fa-music"></i></h2>`,
-`<h2> <i class="fas fa-rainbow"></i> HAPPY BIRTHDAY TO WEATHER GIRL, IZORA ARMSTEAD! HALLELUJAH FOR IZORA! <i class="fas fa-rainbow"></i></h2>`,
-`<h2> <i class="fas fa-bolt"></i> IT'S RAINING MEN WAS RELEASED ON SEPTEMBER 10TH, 1982! TODAY, THE ICONIC SONG TURNS 40! <i class="fas fa-bolt"></i></h2>`,
-`<h2> <i class="fas fa-glasses"></i> HAPPY BIRTHDAY TO PAUL SHAFFER, THE CO-COMPOSER OF IT'S RAINING MEN! <i class="fas fa-glasses"></i></h2>`,
-`<h2> <i class="fas fa-cloud-moon"></i> HAPPY BIRTHDAY TO WEATHER GIRL, MARTHA WASH! HALLELUJAH FOR MARTHA! <i class="fas fa-cloud-moon"></i></h2>`,
-`<h2><i class="fas fa-cloud-sun-rain"></i> TODAY YOU CAN EXPECT ${forecast}! <i class="fas fa-cloud-sun-rain"></i></h2>`,
-`<h2><i class="fas fa-cloud-sun-rain"></i> ENTER THE CITIES YOU WOULD LIKE TO VISIT AND THE WEATHER GIRLS WILL TELL YOU IF IT IS RAINING THERE <i class="fas fa-cloud-sun-rain"></i></h2>`
+  `<h2> <i class="fas fa-music"></i> HAPPY BIRTHDAY TO PAUL JABARA, THE CO-COMPOSER OF IT'S RAINING MEN! <i class="fas fa-music"></i></h2>`,
+  `<h2> <i class="fas fa-rainbow"></i> HAPPY BIRTHDAY TO WEATHER GIRL, IZORA ARMSTEAD! HALLELUJAH FOR IZORA! <i class="fas fa-rainbow"></i></h2>`,
+  `<h2> <i class="fas fa-bolt"></i> IT'S RAINING MEN WAS RELEASED ON SEPTEMBER 10TH, 1982! TODAY, THE ICONIC SONG TURNS 40! <i class="fas fa-bolt"></i></h2>`,
+  `<h2> <i class="fas fa-glasses"></i> HAPPY BIRTHDAY TO PAUL SHAFFER, THE CO-COMPOSER OF IT'S RAINING MEN! <i class="fas fa-glasses"></i></h2>`,
+  `<h2> <i class="fas fa-cloud-moon"></i> HAPPY BIRTHDAY TO WEATHER GIRL, MARTHA WASH! HALLELUJAH FOR MARTHA! <i class="fas fa-cloud-moon"></i></h2>`,
+  `<h2><i class="fas fa-cloud-sun-rain"></i> TODAY YOU CAN EXPECT ${forecast}! <i class="fas fa-cloud-sun-rain"></i></h2>`,
+  `<h2><i class="fas fa-cloud-sun-rain"></i> ENTER THE CITIES YOU WOULD LIKE TO VISIT AND THE WEATHER GIRLS WILL TELL YOU IF IT IS RAINING THERE <i class="fas fa-cloud-sun-rain"></i></h2>`
 ];
 
 // Outputs important days in the breaking news header
-function breakingNews (update, forecast) {
+function breakingNews(update, forecast) {
   forecast === undefined ? forecast = "MAGIC" : forecast;
   // Paul Jabara's Birthday is January 31st
   if (todayDate === "Jan 31st") {
     breaking.innerHTML = breakingNewsMessages[0];
-  // Izora Armstead's Birthday is July 6th  
+    // Izora Armstead's Birthday is July 6th  
   } else if (todayDate === "Jul 6th") {
     breaking.innerHTML = breakingNewsMessages[1];
-  // It's Raining Men's Release Date was September 10th 1982
+    // It's Raining Men's Release Date was September 10th 1982
   } else if (todayDate === "Sep 10th") {
     breaking.innerHTML = breakingNewsMessages[2];
-  // Paul Shaffer's Birthday is November 28th   
+    // Paul Shaffer's Birthday is November 28th   
   } else if (todayDate === "Nov 28th") {
     breaking.innerHTML = breakingNewsMessages[3];
-  // Martha Wash's Birthday is December 28th  
+    // Martha Wash's Birthday is December 28th  
   } else if (todayDate === "Dec 28th") {
     breaking.innerHTML = breakingNewsMessages[4];
-  // forecast message
+    // forecast message
   } else if (update) {
     //forecast;
     breaking.innerHTML = `<h2><i class="fas fa-cloud-sun-rain"></i> TODAY YOU CAN EXPECT ${forecast}! <i class="fas fa-cloud-sun-rain"></i></h2>`;
-  // default message  
+    // default message  
   } else {
     breaking.innerHTML = breakingNewsMessages[6];
   }
@@ -48,12 +49,12 @@ function breakingNews (update, forecast) {
 const cities = [];
 
 // Connects the app to the weather api
-function getAPI (userCity) {
+function getAPI(userCity) {
   // Weather Reporter Header
   let showCity = document.getElementById("city-shown");
   showCity.textContent = `${userCity.toUpperCase()} WEATHER REPORT`;
   // Weather API Extravaganza!
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userCity},us&appid=${APIkey}`) 
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userCity}&units=imperial&appid=${APIkey}`)
     .then(function (response) {
       return response.json();
     })
@@ -71,10 +72,10 @@ function getAPI (userCity) {
 }
 
 // The UV Index requires an One Call API request unlike the other weather information
-function uvIndex (weatherData) {
-  let oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${weatherData.lat}&lon=${weatherData.long}&appid=${APIkey}`;
+function uvIndex(weatherData) {
+  let oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${weatherData.lat}&lon=${weatherData.long}&units=imperial&appid=${APIkey}`;
 
-  fetch(oneCallAPI) 
+  fetch(oneCallAPI)
     .then(function (response) {
       return response.json();
     })
@@ -86,7 +87,7 @@ function uvIndex (weatherData) {
 }
 
 // UV color code
-function uvColorCode () {
+function uvColorCode() {
   let getUV = document.querySelectorAll(".uv-index");
   for (let i = 0; i < getUV.length; i++) {
     const currentUV = parseFloat(getUV[i].textContent);
@@ -109,7 +110,7 @@ function currentWeatherSearch(weather) {
     lat: weather.coord.lat,
     long: weather.coord.lon,
     icon: weather.weather[0].icon,
-    temp: celsiusToFahrenheit(weather.main.temp),
+    temp: weather.main.temps,
     wind: weather.wind.speed,
     humidity: weather.main.humidity,
     uvIndex: 0
@@ -118,7 +119,7 @@ function currentWeatherSearch(weather) {
 }
 
 // Outputs the weather forecast to the page
-function displayWeather (weather) {
+function displayWeather(weather) {
   let getCards = document.querySelectorAll(".card");
   for (let i = 0; i < getCards.length; i++) {
     const currentCard = getCards[i];
@@ -127,14 +128,14 @@ function displayWeather (weather) {
     // needed information for each item
     const dateString = moment.unix(weather.daily[i].dt).format("MMM Do YYYY");
     const daysIcon = weather.daily[i].weather[0].icon;
-    const daysTemp = weather.daily[i].temp.day;
+    const daysTemp = Math.ceil(weather.daily[i].temp.day) + "°F";
     const daysWind = weather.daily[i].wind_speed;
     const daysHumidity = weather.daily[i].humidity;
     let daysUVIndex = weather.daily[i].uvi;
     // content for each card
     currentCard.childNodes[1].textContent = dateString;
     weatherIcon[i].setAttribute("src", `http://openweathermap.org/img/wn/${daysIcon}.png`);
-    currentCard.children[1].children[1].children[0].children[0].textContent = celsiusToFahrenheit(daysTemp);
+    currentCard.children[1].children[1].children[0].children[0].textContent = daysTemp;
     currentCard.children[1].children[1].children[1].children[0].textContent = ` ${daysWind} MPH`;
     currentCard.children[1].children[1].children[2].children[0].textContent = ` ${daysHumidity} %`;
     currentCard.children[1].children[1].children[3].children[0].textContent = daysUVIndex;
@@ -143,34 +144,28 @@ function displayWeather (weather) {
 }
 
 // Saves user's past city inputs and makes them buttons
-function pastWeatherSearches (recentSearch) {
+function pastWeatherSearches(recentSearch) {
   cities.push(recentSearch);
   localStorage.setItem("Past Searches", cities);
   recentSearch = recentSearch.toUpperCase();
   let citiesSearched = localStorage.getItem("Past Searches");
   let currentCityText = recentSearch;
   let currentCityButton = document.createElement("button");
-    
+
   currentCityButton.setAttribute("class", ".btn-history");
   currentCityButton.textContent = currentCityText;
   citiesSearchList.appendChild(currentCityButton);
 }
 
-// Converts the temperature Celsius to Fahrenheit
-function celsiusToFahrenheit (celTemp) {
-  let convertedTemp =  (celTemp * 9 )/ (5 + 32);
-  return Math.ceil(convertedTemp) + "°F";
-}
-
 window.addEventListener("load", breakingNews)
-getWeatherBtn.addEventListener("click", function(event) {
+getWeatherBtn.addEventListener("click", function (event) {
   event.preventDefault();
   let userCity = document.getElementById("user-city").value;
   getAPI(userCity);
   pastWeatherSearches(userCity);
 });
 
-citiesSearchList.addEventListener("click", function(event) {
+citiesSearchList.addEventListener("click", function (event) {
   let cityName = event.target.textContent.toLowerCase();
   getAPI(cityName);
 });
